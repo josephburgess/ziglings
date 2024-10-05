1a8d15072266f3abb8588d4701fe6312d685d246
josephburgess <josephburgess@gmail.com>
2024-10-05 20:46:52 +0100

    loops etc

--- exercises/015_for.zig
+++ exercises/015_for.zig
@@ -1,13 +1,3 @@
-//
-// Behold the 'for' loop! For loops let you execute code for each
-// element of an array:
-//
-//     for (items) |item| {
-//
-//         // Do something with item
-//
-//     }
-//
 const std = @import("std");
 
 pub fn main() void {
@@ -15,7 +5,7 @@ pub fn main() void {
 
     std.debug.print("A Dramatic Story: ", .{});
 
-    for (???) |???| {
+    for (story) |scene| {
         if (scene == 'h') std.debug.print(":-)  ", .{});
         if (scene == 's') std.debug.print(":-(  ", .{});
         if (scene == 'n') std.debug.print(":-|  ", .{});
@@ -23,9 +13,3 @@ pub fn main() void {
 
     std.debug.print("The End.\n", .{});
 }
-// Note that 'for' loops also work on things called "slices"
-// which we'll see later.
-//
-// Also note that 'for' loops have recently become more flexible
-// and powerful (two years after this exercise was written).
-// More about that in a moment.
