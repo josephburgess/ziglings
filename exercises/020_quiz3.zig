a07e4bec4da2f2250fd5d42513940ee733086438
josephburgess <josephburgess@gmail.com>
2024-10-05 21:02:23 +0100

    functions 2 and quiz

--- exercises/020_quiz3.zig
+++ exercises/020_quiz3.zig
@@ -1,10 +1,3 @@
-//
-// Let's see if we can make use of some of the things we've learned so far.
-// We'll create two functions: one that contains a "for" loop and one
-// that contains a "while" loop.
-//
-// Both of these are simply labeled "loop" below.
-//
 const std = @import("std");
 
 pub fn main() void {
@@ -14,30 +7,19 @@ pub fn main() void {
     std.debug.print("\n", .{});
 }
 
-// You won't see this every day: a function that takes an array with
-// exactly four u16 numbers. This is not how you would normally pass
-// an array to a function. We'll learn about slices and pointers in
-// a little while. For now, we're using what we know.
-//
-// This function prints, but does not return anything.
-//
-fn printPowersOfTwo(numbers: [4]u16) ??? {
-    loop (numbers) |n| {
+fn printPowersOfTwo(numbers: [4]u16) void {
+    for (numbers) |n| {
         std.debug.print("{} ", .{twoToThe(n)});
     }
 }
 
-// This function bears a striking resemblance to twoToThe() in the last
-// exercise. But don't be fooled! This one does the math without the aid
-// of the standard library!
-//
-fn twoToThe(number: u16) ??? {
+fn twoToThe(number: u16) u16 {
     var n: u16 = 0;
     var total: u16 = 1;
 
-    loop (n < number) : (n += 1) {
+    while (n < number) : (n += 1) {
         total *= 2;
     }
 
-    return ???;
+    return total;
 }
